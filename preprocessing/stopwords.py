f660f5ee0bc8f4ad5a128c14528f902a9d4cb73e
arda190 <aardaa_karakas@outlook.com>
2026-07-26 16:10:24 +0300

    add stopwords for text preprocessing

---
+++ preprocessing/stopwords.py
@@ -0,0 +1,99 @@
+STOP_WORDS = {
+    # Articles
+    "a", "an", "the",
+
+    # Pronouns
+    "i", "me", "my", "mine", "myself",
+    "you", "your", "yours", "yourself", "yourselves",
+    "he", "him", "his", "himself",
+    "she", "her", "hers", "herself",
+    "it", "its", "itself",
+    "we", "us", "our", "ours", "ourselves",
+    "they", "them", "their", "theirs", "themselves",
+
+    # Demonstratives
+    "this", "that", "these", "those",
+
+    # Auxiliary / Be verbs
+    "am", "is", "are", "was", "were",
+    "be", "been", "being",
+
+    # Have verbs
+    "have", "has", "had", "having",
+
+    # Do verbs
+    "do", "does", "did", "doing",
+
+    # Modal verbs
+    "can", "could",
+    "will", "would",
+    "shall", "should",
+    "may", "might",
+    "must",
+
+    # Common verbs
+    "get", "gets", "got",
+    "make", "makes", "made",
+    "take", "takes", "taken",
+    "go", "goes", "went", "gone",
+
+    # Conjunctions
+    "and", "or", "but",
+    "because", "if", "while",
+    "although", "though",
+    "since", "unless",
+
+    # Prepositions
+    "of", "to", "in", "on", "at", "by",
+    "for", "from", "with", "without",
+    "into", "onto",
+    "about", "against",
+    "between", "among",
+    "before", "after",
+    "during", "through",
+    "under", "over",
+    "above", "below",
+    "within", "outside",
+
+    # Direction
+    "up", "down", "out", "off",
+
+    # Determiners / Quantifiers
+    "all", "any", "both",
+    "each", "every",
+    "few", "many",
+    "more", "most",
+    "much", "some",
+    "such", "other",
+    "another", "own",
+    "same",
+
+    # Adverbs
+    "very", "too",
+    "just", "only",
+    "also", "even",
+    "still", "already",
+    "again",
+
+    # Time / Place
+    "here", "there",
+    "when", "where",
+    "then", "now",
+    "once",
+
+    # Question words
+    "what", "which",
+    "who", "whom",
+    "whose",
+    "why",
+    "how",
+
+    # Negatives
+    "no", "nor",
+    "not",
+
+    # Miscellaneous
+    "as", "than",
+    "so", "via",
+    "per"
+}
