e428b6c19da28e983c7c6f3c83e83ec868c53e6e
arda190 <aardaa_karakas@outlook.com>
2026-07-27 10:08:06 +0300

    updated LogisticRegression weights features and added save_model

--- models/LogisticRegression.py
+++ models/LogisticRegression.py
@@ -1,52 +1,70 @@
+import json
 import math
 class LogisticRegression:
     def __init__(self):
-        self.learning_rate=0.01
-        self.bias=0
-        self.weight=0
+        self.learning_rate = 0.01
+        self.bias = 0
+        self.weights = []
 
 
     def sigmoid(self,x):
-        return  1/(1+math.exp(-x))        # 1/(1+e^-x)
+        return 1/(1+math.exp(-x))
 
+
+    def saveToFile(self):
+        pass
 
 
     def predict(self,x):
-        prediction = self.bias + self.weight * x
+        prediction = self.bias
+
+        for x1,w1 in zip(x,self.weights):
+            prediction += w1*x1
 
         return self.sigmoid(prediction)
 
-    def classify(self,x):
-       prediction = self.predict(x)
-       if prediction>=0.5:
-           return 1
-       else:
-           return 0
+
+    def predict_class(self, x):
+        prediction =self.predict(x)
+
+        if prediction > 0.5:
+            return 1
+        else:
+            return 0
 
 
-
-    def log_loss(self,p,y):
-        return - ( y * math.log(p) + (1-y) * math.log(1-p) )
 
 
     def fit(self,x_,y_):
+        self.weights = [0] * len(x_[0])  # [ [1,1,3,4,2] , [6,7,8,9,4]  ]   [  [2,2,4,5,3] , [7,8,9,10,5] ]
 
+        for epoch in range(1000):
 
-        for epoch in range(10000):
-            dw=0
-            db=0
+           dw = [0] * len(x_[0])
+           db = 0
 
-            for x,actual_y in zip(x_,y_):
-                prediction = self.predict(x)
+           for x , actual_y in zip(x_,y_):
+               # logistic regression use log loss
 
-                loss=self.log_loss(prediction,actual_y)
+              p=self.predict(x)
 
-                dw = dw + ( x * (prediction - actual_y) )
-                db = db + (prediction - actual_y)
+              for i in range(len(x)):
+                  dw[i] += (p - actual_y) * x[i]
 
-            dw = dw/len(x_)
-            db = db/len(y_)
+              db += (p - actual_y)
 
-            self.weight = self.weight - self.learning_rate * dw
-            self.bias = self.bias - self.learning_rate * db
+           db /=len(x_)
+           self.bias = self.bias - self.learning_rate * db
 
+           for i in range(len(self.weights)):
+               dw[i] /= len(x_)
+               self.weights[i] =self.weights[i] - self.learning_rate * dw[i]
+
+    def save_model(self, filename):
+        data = {
+            "weights": self.weights,
+            "bias": self.bias
+        }
+
+        with open(filename, "w") as f:
+            json.dump(data, f, indent=4)
