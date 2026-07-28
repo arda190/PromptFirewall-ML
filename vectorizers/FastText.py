373059451060508c4a70b8a702166b1cdc61ea2f
arda190 <aardaa_karakas@outlook.com>
2026-07-28 13:14:05 +0300

    add mean pooling method for FastText

--- vectorizers/FastText.py
+++ vectorizers/FastText.py
@@ -1,33 +1,55 @@
 from gensim.models import FastText
+import numpy as np
 
 class FastTextVectorizer:
-    def __init__(self, vector_size=100):
-        self.model = None
-        self.vector_size = vector_size
+    def __init__(self,vector_size=100):
+        self.model=None
+        self.vector_size =vector_size
 
 
 
-    def fit(self, sentences):
+    def fit(self,sentences):
+        tokenized = [sentence.split() for sentence in sentences]
         self.model = FastText(
-            sentences=sentences,
+            sentences=tokenized,
             vector_size=self.vector_size,
             window=5,
             min_count=1
         )
 
 
-    def transform(self, sentences):
-        pass
+    def transform(self,sentences):
+        vectors = []
+
+        for sentence in sentences:
+            word_vectors = []
+
+            for word in sentence.split():
+                if word in self.model.wv:
+                    word_vectors.append(self.model.wv[word])
+
+            if len(word_vectors) ==0:
+                vectors.append(np.zeros(self.vector_size))
+            else:
+                vectors.append(np.mean(word_vectors,axis=0))
+
+        return vectors
+
+
+
+    def fit_transform(self,sentences):
+        self.fit(sentences)
+        return self.transform(sentences)
 
 
 
 
-    def save_model(self, filename):
+    def save_model(self,filename):
         self.model.save(filename)
 
 
-    def load_model(self, filename):
-        self.model = FastText.load(filename)
+    def load_model(self,filename):
+        self.model=FastText.load(filename)
 
 
 
