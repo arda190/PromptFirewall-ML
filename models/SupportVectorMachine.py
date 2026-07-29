8b6851f4777ec1ef7a2e1405c2a223901c392a91
arda190 <aardaa_karakas@outlook.com>
2026-07-29 16:00:58 +0300

    add SVM model and update requirements.txt

---
+++ models/SupportVectorMachine.py
@@ -0,0 +1,30 @@
+from sklearn.svm import SVC
+import joblib
+
+class SupportVectorMachine:
+    def __init__(self,kernel="linear"):
+        self.kernel = kernel
+        self.model = None
+
+
+
+
+    def fit(self,x,y):
+        self.model = SVC(kernel=self.kernel,probability=True)
+        self.model.fit(x,y)
+
+
+    def predict(self,x):
+        return self.model.predict(x)
+
+
+    def save_model(self,path):
+        joblib.dump(self.model,path)
+
+
+    def load_model(self,path):
+        self.model = joblib.load(path)
+
+
+    def score(self,x,y):
+        return self.model.score(x,y)
