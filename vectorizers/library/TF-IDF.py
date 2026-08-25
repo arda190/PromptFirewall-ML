56500b3770b6f3b6f8642c67dcf2a3f52895f750
arda190 <aardaa_karakas@outlook.com>
2026-08-25 17:47:38 +0300

    add tf-idf library version

---
+++ vectorizers/library/TF-IDF.py
@@ -0,0 +1,29 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import joblib
+
+
+class TFIDFVectorizer:
+    def __init__(self,max_features=None,min_df=1, max_df=1.0,ngram_range=(3,5)):
+        self.vectorizer = TfidfVectorizer(
+            max_features=max_features,
+            min_df=min_df,
+            max_df=max_df,
+            ngram_range=ngram_range
+        )
+
+    def fit(self, sentences):
+        self.vectorizer.fit(sentences)
+        return self
+
+    def transform(self, sentences):
+        return self.vectorizer.transform(sentences)
+
+    def fit_transform(self, sentences):
+        return self.vectorizer.fit_transform(sentences)
+
+    def save_model(self, filename):
+        joblib.dump(self.vectorizer, filename)
+
+    def load_model(self, filename):
+        self.vectorizer = joblib.load(filename)
+        return self
