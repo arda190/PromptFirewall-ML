1e0ffaac7732a055effba557dea06cd1f1543e86
arda190 <aardaa_karakas@outlook.com>
2026-08-31 13:50:35 +0300

    add type hints and update sentence vector logic

--- vectorizers/library/FastText.py
+++ vectorizers/library/FastText.py
@@ -2,63 +2,64 @@ from gensim.models import FastText
 import numpy as np
 
 class FastTextVectorizer:
-    def __init__(self,vector_size=100):
-        self.model=None
-        self.vector_size =vector_size
-        self.name = "FastText"
 
+  def __init__(self, vector_size=100, window=5, min_count=1, workers=4, **kwargs):
+    self.model = None
+    self.vector_size = vector_size
+    self.window = window
+    self.min_count = min_count
+    self.workers = workers
+    self.extra_kwargs = kwargs
+    self.name = "FastText"
 
+  def fit(self, sentences:list[str])->"FastTextVectorizer" :
+    tokenized = [sentence.split() for sentence in sentences]
+    self.model = FastText(
+        sentences=tokenized,
+        vector_size=self.vector_size,
+        window=self.window,
+        min_count=self.min_count,
+        workers=self.workers,
+        **self.extra_kwargs,
+    )
+    return self
 
-    def fit(self,sentences):
-        tokenized = [sentence.split() for sentence in sentences]
-        self.model = FastText(
-            sentences=tokenized,
-            vector_size=self.vector_size,
-            window=5,
-            min_count=1
-        )
+  def transform(self, sentences:list[str])->np.ndarray:
+    if self.model is None:
+      raise ValueError("FastTextVectorizer has not been trained yet.")
 
+    vectors = []
+    for sentence in sentences:
+        words = sentence.split()
+        if not words:
+            vectors.append(np.zeros(self.vector_size))
+            continue
 
-    def transform(self,sentences):
+        word_vectors = []
+        for word in words:
+            try:
+                word_vectors.append(self.model.wv[word])
+            except KeyError:
+                pass
 
-        if self.model is None:
-            raise ValueError("FastTextVectorizer has not been trained yet.")
+        if len(word_vectors) == 0:
+            vectors.append(np.zeros(self.vector_size))
+        else:
+            vectors.append(np.mean(word_vectors, axis=0))
 
-        vectors = []
+    return np.array(vectors)
 
-        for sentence in sentences:
-            word_vectors = []
+  def fit_transform(self, sentences:list[str])->np.ndarray:
+    self.fit(sentences)
+    return self.transform(sentences)
 
-            for word in sentence.split():
-                if word in self.model.wv:
-                    word_vectors.append(self.model.wv[word])
+  def save_model(self, filename:str)->None:
+    if self.model is None:
+        raise ValueError("FastTextVectorizer has not been trained yet.")
+    self.model.save(filename)
 
-            if len(word_vectors) ==0:
-                vectors.append(np.zeros(self.vector_size))
-            else:
-                vectors.append(np.mean(word_vectors,axis=0))
+  def load_model(self, filename="saved_models/fasttext")->"FastTextVectorizer":
+    self.model = FastText.load(filename)
 
-        return vectors
-
-
-
-    def fit_transform(self,sentences):
-        self.fit(sentences)
-        return self.transform(sentences)
-
-
-
-
-    def save_model(self,filename):
-        self.model.save(filename)
-
-
-    def load_model(self,filename="saved_models/fasttext"):
-        self.model=FastText.load(filename)
-        print("model loaded")
-        return self
-
-
-
-
-
+    self.vector_size = self.model.vector_size
+    return self
